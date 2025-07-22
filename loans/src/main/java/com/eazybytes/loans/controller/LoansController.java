f28a96a0fd84f38b6ba0e4526aaa2012ca96fe48
Nguyen Minh Dat <nmdat21@clc.fitus.edu.vn>
2025-07-23 00:28:19 +0700

    Add CRUD REST API

---
+++ loans/src/main/java/com/eazybytes/loans/controller/LoansController.java
@@ -0,0 +1,76 @@
+package com.eazybytes.loans.controller;
+
+import com.eazybytes.loans.constants.LoansConstants;
+import com.eazybytes.loans.dto.LoansDto;
+import com.eazybytes.loans.dto.ResponseDto;
+import com.eazybytes.loans.service.ILoansService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
+@AllArgsConstructor
+@Validated
+public class LoansController {
+    private final ILoansService iLoansService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createLoan(
+            @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+            @RequestParam
+            String mobileNumber
+    ) {
+        iLoansService.createLoan(mobileNumber);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(LoansConstants.STATUS_201, LoansConstants.MESSAGE_201));
+    }
+
+    @GetMapping("/fetch")
+    public ResponseEntity<LoansDto> fetchLoan(
+            @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+            @RequestParam
+            String mobileNumber
+    ) {
+        LoansDto loansDto = iLoansService.fetchLoan(mobileNumber);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(loansDto);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateLoan(@Valid @RequestBody LoansDto loansDto) {
+        boolean isUpdated = iLoansService.updateLoan(loansDto);
+        if (isUpdated)
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(LoansConstants.STATUS_200, LoansConstants.MESSAGE_200));
+        else
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(LoansConstants.STATUS_417, LoansConstants.MESSAGE_417_UPDATE));
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDto> deleteLoan(
+            @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+            @RequestParam
+            String mobileNumber
+    ) {
+        boolean isDeleted = iLoansService.deleteLoan(mobileNumber);
+        if (isDeleted)
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(LoansConstants.STATUS_200, LoansConstants.MESSAGE_200));
+        else
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(LoansConstants.STATUS_417, LoansConstants.MESSAGE_417_DELETE));
+    }
+}
---
+++ loans/src/main/java/com/eazybytes/loans/service/impl/LoansServiceImpl.java
@@ -0,0 +1,69 @@
+package com.eazybytes.loans.service.impl;
+
+import com.eazybytes.loans.constants.LoansConstants;
+import com.eazybytes.loans.dto.LoansDto;
+import com.eazybytes.loans.entity.Loans;
+import com.eazybytes.loans.exception.LoanAlreadyExistsException;
+import com.eazybytes.loans.exception.ResourceNotFoundException;
+import com.eazybytes.loans.mapper.LoansMapper;
+import com.eazybytes.loans.repository.LoansRepository;
+import com.eazybytes.loans.service.ILoansService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
+public class LoansServiceImpl implements ILoansService {
+    private final LoansRepository loansRepository;
+
+    @Override
+    public void createLoan(String mobileNumber) {
+        Optional<Loans> optionalLoans = loansRepository.findByMobileNumber(mobileNumber);
+        if (optionalLoans.isPresent())
+            throw new LoanAlreadyExistsException("Loan already registered with given mobile number: " + mobileNumber);
+        loansRepository.save(createNewLoan(mobileNumber));
+    }
+
+    @Override
+    public LoansDto fetchLoan(String mobileNumber) {
+        Loans loans = loansRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Loan", "mobileNumber", mobileNumber)
+        );
+        return LoansMapper.mapToLoansDto(loans, new LoansDto());
+    }
+
+    @Override
+    public boolean updateLoan(LoansDto loansDto) {
+        String loanNumber = loansDto.getLoanNumber();
+        Loans loans = loansRepository.findByLoanNumber(loanNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Loan", "loanNumber", loanNumber)
+        );
+        LoansMapper.mapToLoans(loansDto, loans);
+        loansRepository.save(loans);
+        return true;
+    }
+
+    @Override
+    public boolean deleteLoan(String mobileNumber) {
+        Loans loans = loansRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Loan", "mobileNumber", mobileNumber)
+        );
+        loansRepository.deleteById(loans.getLoanId());
+        return true;
+    }
+
+    private Loans createNewLoan(String mobileNumber) {
+        Loans newLoan = new Loans();
+        newLoan.setMobileNumber(mobileNumber);
+        Long randomLoanNumber = 100_000_000_000L + new Random().nextInt(900_000_000);
+        newLoan.setLoanNumber(Long.toString(randomLoanNumber));
+        newLoan.setLoanType(LoansConstants.HOME_LOAN);
+        newLoan.setTotalLoan(LoansConstants.NEW_LOAN_LIMIT);
+        newLoan.setAmountPaid(0);
+        newLoan.setOutstandingAmount(LoansConstants.NEW_LOAN_LIMIT);
+        return newLoan;
+    }
+}
