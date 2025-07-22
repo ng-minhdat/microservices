f55c9839cfea9fcbd862f971e05f30fba631e562
Nguyen Minh Dat <nmdat21@clc.fitus.edu.vn>
2025-07-23 00:24:11 +0700

    Modify updatedAt & updatedBy columns inside loan schema

--- loans/src/main/resources/schema.sql
+++ loans/src/main/resources/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS `loans` (
     `outstanding_amount` int NOT NULL,
     `created_at` date NOT NULL,
     `created_by` varchar(20) NOT NULL,
-    `updated_at` date NOT NULL,
-    `updated_by` varchar(20) NOT NULL,
+    `updated_at` date DEFAULT NULL,
+    `updated_by` varchar(20) DEFAULT NULL,
     PRIMARY KEY(`loan_id`)
 );
