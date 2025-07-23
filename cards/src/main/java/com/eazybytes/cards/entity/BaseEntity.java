703f8762fcaeab90faee657dc6ed8299961ca0ba
Nguyen Minh Dat <nmdat21@clc.fitus.edu.vn>
2025-07-23 10:10:55 +0700

    Modify auditting annotation

--- cards/src/main/java/com/eazybytes/cards/entity/BaseEntity.java
+++ cards/src/main/java/com/eazybytes/cards/entity/BaseEntity.java
@@ -10,12 +10,13 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter @Setter @ToString
-@EntityListeners(EntityListeners.class)
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
     @CreatedDate
     @Column(updatable = false)
